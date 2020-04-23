b61857ca911bd072a6b0d6d31892d1ba09fff874
Gabi <csgabka@gmail.com>
2020-04-23 21:20:27 +0100

    speech

--- script.js
+++ script.js
@@ -61,15 +61,82 @@ data.forEach(createBox);
 //Create Speech Box
 
 function createBox(item) {
-	const box = document.createElement('div');
+  const box = document.createElement('div');
 
-	const { image, text } = item;
-	box.classList.add('box');
-	box.innerHTML = `
-	<img src="${image}" alt="${text}" />
-	<p class="info">${text}</p>
-	`;
+  const { image, text } = item;
 
-	// @todo - speak event
-	main.appendChild(box);
+  box.classList.add('box');
+
+  box.innerHTML = `
+    <img src="${image}" alt="${text}" />
+    <p class="info">${text}</p>
+  `;
+
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add active effect
+    box.classList.add('active');
+    setTimeout(() => box.classList.remove('active'), 800);
+  });
+
+  main.appendChild(box);
 }
+
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// Store voices
+let voices = [];
+
+function getVoices() {
+	voices = speechSynthesis.getVoices();
+
+	voices.forEach(voice => {
+		const option = document.createElement('option');
+
+		option.value = voice.name;
+		option.innerText = `${voice.name} ${voice.lang}`;
+
+		voicesSelect.appendChild(option);
+	});
+}
+
+//Set text
+function setTextMessage(text) {
+	message.text = text;
+}
+
+//Speak text
+function speakText() {
+	speechSynthesis.speak(message);
+}
+
+//Set Voice
+function setVoice(e) {
+	message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
+//Voices changed
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+//Toggle text box 
+toggleBtn.addEventListener('click', () => document.getElementById('text-box')
+	.classList.toggle('show')
+);
+
+//Close button
+closeBtn.addEventListener('click', () => document.getElementById('text-box').classList.remove('show'));
+
+
+//Change Voice
+voicesSelect.addEventListener('change', setVoice);
+
+//Read text button
+readBtn.addEventListener('click', () => {
+	setTextMessage(textarea.value);
+	speakText();
+});
+
+getVoices();
